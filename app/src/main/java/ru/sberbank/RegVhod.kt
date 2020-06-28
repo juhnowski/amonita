740cb94f309c4ca1516450e3f1b6169e8c515e06
Juhnowski <juhnowski@gmail.com>
2020-06-29 02:16:42 +0300

    add reg vhod

---
+++ app/src/main/java/ru/sberbank/RegVhod.kt
@@ -0,0 +1,11 @@
+package ru.sberbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegVhod : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_vhod)
+    }
+}
--- app/src/main/java/ru/sberbank/SplashScreen.kt
+++ app/src/main/java/ru/sberbank/SplashScreen.kt
@@ -1,5 +1,6 @@
 package ru.sberbank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+//        Thread.sleep(5000)
+//        val intent = Intent(this, RegVhod::class.java)
+//        startActivity(intent)
     }
 }
