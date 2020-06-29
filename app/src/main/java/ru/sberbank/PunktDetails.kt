481731c2758a88c70e2f4a22a8a6b89dd28b06c1
Juhnowski <juhnowski@gmail.com>
2020-06-29 08:41:12 +0300

    add documents, photo-video, favorites actions to details punkts

--- app/src/main/java/ru/sberbank/PunktDetails.kt
+++ app/src/main/java/ru/sberbank/PunktDetails.kt
@@ -1,12 +1,30 @@
 package ru.sberbank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class PunktDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_punkt_details)
         setTitle("ООО ПРОГРЕСС")
+    }
+
+    fun show_documents(v:View){
+        val intent = Intent(this, Documents::class.java)
+        startActivity(intent)
+    }
+
+    fun show_presentation(v:View){
+        val intent = Intent(this, Presentation::class.java)
+        startActivity(intent)
+    }
+
+    fun add_to_favorites(v:View){
+        Snackbar.make(v, "Закупочный пункт добавлен в Избранное", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
     }
 }
